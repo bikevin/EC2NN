ef5009b63afa49ccce4218b0d51dc9d045effa3e
bikevin <kevin.bi.1002@gmail.com>
2016-08-12 13:45:15 -0400

    Create net_predictor.py

---
+++ python/net_predictor.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('/home/ubuntu/caffe-master/python')
+import caffe
+import os.path
+import rsquared as r2
+from pylab import *
+
+caffe.set_mode_cpu()
+
+if len(sys.argv) != 5:
+        exit("Error: Incorrect number of arguments. \nUsage: net_predictor.py <file path to model prototxt> <filepath to caffemodel file> <filepath to testing data> <userdir>")
+        
+if not os.path.isfile(sys.argv[4]):
+        exit("Error: Invalid userdir")
+        
+netFilePath = sys.argv[4] + '/' + sys.argv[1]
+modelFilePath = sys.argv[4] + '/' + sys.argv[2]
+testFilePath = sys.argv[4] + '/' + sys.argv[3]
+
+if not os.path.isfile(netFilePath):
+	exit("Error: File path to net prototxt is invalid.")
+if not os.path.isfile(modelFilePath):
+	exit("Error: File path to .caffemodel file is invalid.")
+if not os.path.isfile(testFilePath):
+	exit("Error: File path to testing data is invalid.")
+
+data = h5py.File(testFilePath, 'r')
+
+net = caffe.Net(str(netFilePath), str(modelFilePath), caffe.TEST)
